029219adaa9ce78fbdf949a2bf31e7e3e6d8dc7b
Mihai Esanu <mcesanu@gmail.com>
2024-01-10 17:04:04 -0700

    Adding some basic configs for the IMU controller and removing some vestigial headers

--- components/imu_controller/imu_controller.c
+++ components/imu_controller/imu_controller.c
@@ -1,9 +1,6 @@
 #include "imu_controller_api.h"
 #include "imu_controller_ext.h"
-#include "esp_log.h"
 #include "sdkconfig.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 
 BaseType_t IMUControllerInit(void) {
     return pdFALSE;
