50c73958af8e46eb3903e91966a98c6b77f74b83
Mihai Esanu <mcesanu@gmail.com>
2024-01-21 12:18:02 -0700

    Remove network controller component
    Remove the network controller component and move wifi functionality to the
    main task

--- components/network_controller/network_controller_api.h
+++
@@ -1,7 +0,0 @@
-#ifndef NETWORK_CONTROLLER_API_H
-#define NETWORK_CONTROLLER_API_H
-
-void test_network(int i);
-void network_controller_init(void);
-
-#endif /* NETWORK_CONTROLLER_API_H */
--- main/main.c
+++ main/main.c
@@ -7,7 +7,13 @@
 #include "driver/gpio.h"
 #include "bmi270.h"
 
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
 #include "esp_log.h"
+#include "nvs_flash.h"
+
+#include "sdkconfig.h"
 
 #define WIFI_CONNECTED_BIT BIT0
 #define WIFI_FAIL_BIT      BIT1
@@ -21,6 +27,18 @@
 #define ESP_INTR_FLAG_DEFAULT 0
 
 static char *TAG = "main";
+static uint8_t wifiRetryNum = 0;
+static EventGroupHandle_t wifiConnectEventGroup;
+
+static wifi_config_t wifiConfig = {
+    .sta = {
+        .ssid = CONFIG_NETWORK_CONTROLLER_WIFI_SSID,
+        .password = CONFIG_NETWORK_CONTROLLER_WIFI_PASSWORD,
+        .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        .sae_pwe_h2e = WPA3_SAE_PWE_BOTH,
+        .sae_h2e_identifier = "",
+    },
+};
 
 static spi_bus_config_t busConfig = {
     .miso_io_num = PIN_NUM_MISO,
@@ -30,10 +48,88 @@ static spi_bus_config_t busConfig = {
     .quadhd_io_num = -1,
 };
 
+static void wifiConnectEventHandler(void* arg, esp_event_base_t eventBase,
+                                int32_t eventId, void* eventData) {
+    if (eventBase == WIFI_EVENT && eventId == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (eventBase == WIFI_EVENT && eventId == WIFI_EVENT_STA_DISCONNECTED) {
+        if (wifiRetryNum < CONFIG_NETWORK_CONTROLLER_MAXIMUM_RETRY) {
+            esp_wifi_connect();
+            wifiRetryNum++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(wifiConnectEventGroup, WIFI_FAIL_BIT);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (eventBase == IP_EVENT && eventId == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) eventData;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        wifiRetryNum = 0;
+        xEventGroupSetBits(wifiConnectEventGroup, WIFI_CONNECTED_BIT);
+    }
+}
+
+static void wifiInit(void) {
+    wifiConnectEventGroup = xEventGroupCreate();
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instanceAnyId;
+    esp_event_handler_instance_t instanceGotIp;
+
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifiConnectEventHandler,
+                                                        NULL,
+                                                        &instanceAnyId));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifiConnectEventHandler,
+                                                        NULL,
+                                                        &instanceGotIp));
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifiConfig) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    EventBits_t bits = xEventGroupWaitBits(wifiConnectEventGroup,
+            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED_BIT) {
+        ESP_LOGI(TAG, "connected to ap SSID:%s password:%s",
+                 CONFIG_NETWORK_CONTROLLER_WIFI_SSID, CONFIG_NETWORK_CONTROLLER_WIFI_PASSWORD);
+    } else if (bits & WIFI_FAIL_BIT) {
+        ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+                 CONFIG_NETWORK_CONTROLLER_WIFI_SSID, CONFIG_NETWORK_CONTROLLER_WIFI_PASSWORD);
+    } else {
+        ESP_LOGE(TAG, "UNEXPECTED EVENT");
+    }
+}
+
 void app_main(void)
 {
     uint8_t imuIndex;
     IMUFIFOData_t *data;
+
+    //Initialize NVS
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+
+    wifiInit();
+
     spi_bus_initialize(SPI2_HOST, &busConfig, SPI_DMA_CH_AUTO);
 
     gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
